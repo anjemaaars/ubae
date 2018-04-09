06eb6c13edcb6ff3cc3e02df5765a431870b636d
earmix <earmix@outlook.com>
2018-04-09 23:53:37 +0800

    .

--- routes/main.js
+++ routes/main.js
@@ -15,7 +15,35 @@ router.get('/', function(req, res, next) {
 
 /* GET route page */
 router.get('/examination/first', function(req,res) {
-  res.render('exam');
+  var resource = {
+    data: [{
+      date: 'March 22, 2018',
+      scheme: 'even', //odd or even
+      interval: '90', //minutes to determine gap
+      start: '9:30',
+      end: '8:00',
+    }, {
+      date: 'March 23, 2018',
+      scheme: 'even', //odd or even
+      interval: '60', //minutes to determine gap
+      start: '9:00',
+      end: '8:00',
+    }, {
+      date: 'March 26, 2018',
+      scheme: 'odd', //odd or even
+      interval: '60', //minutes to determine gap
+      start: '8:00',
+      end: '7:00',
+    }, {
+      date: 'March 27, 2018',
+      scheme: 'odd', //odd or even
+      interval: '90', //minutes to determine gap
+      start: '8:00',
+      end: '6:30',
+    }]
+  }
+
+  res.render('exam', resource);
 });
 
 /* GET route page */
@@ -53,7 +81,35 @@ router.get('/examination/midterm', function(req,res) {
 
 /* GET route page */
 router.get('/examination/final', function(req,res) {
-  res.render('exam');
+  var resource = {
+    data: [{
+      date: 'March 22, 2018',
+      scheme: 'even', //odd or even
+      interval: '90', //minutes to determine gap
+      start: '9:30',
+      end: '8:00',
+    }, {
+      date: 'March 23, 2018',
+      scheme: 'even', //odd or even
+      interval: '60', //minutes to determine gap
+      start: '9:00',
+      end: '8:00',
+    }, {
+      date: 'March 26, 2018',
+      scheme: 'odd', //odd or even
+      interval: '60', //minutes to determine gap
+      start: '8:00',
+      end: '7:00',
+    }, {
+      date: 'March 27, 2018',
+      scheme: 'odd', //odd or even
+      interval: '90', //minutes to determine gap
+      start: '8:00',
+      end: '6:30',
+    }]
+  }
+
+  res.render('exam', resource);
 });
 
 module.exports = router;
