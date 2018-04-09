8cace00ae468ed9d0bf9ee335c2e8866e23b12d5
earmix <earmix@outlook.com>
2018-04-09 20:03:06 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  var web_page = process.env.WEB_PAGE_URL;
+  var resource = {
+    title: 'UBAE',
+    url: process.env.WEB_PAGE_URL
+  };
 
-  res.render('index', { title: 'Express', url: web_page });
+  res.render('index', resource);
 });
 
 module.exports = router;
